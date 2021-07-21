3dbe6e2e8d74ae68ea9e528c86e0d6810fc9f0f8
K Andy <kdnanderson94@gmail.com>
2021-07-21 23:03:41 +0100

    beginning of building search field

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 baguetteBox.run('.gallery');
 
+let input
+let filter 
+let pic
+let caption
+
+input = document.getElementById('myInput').addEventListener(InputEvent, console.log());
+
+
+//timer for help
+setTimeout(function() { alert("my message"); }, 2000);
+console.log(input)
 
