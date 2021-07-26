9b34cf2ae881f065007093c371c32634d51913b6
K Andy <kdnanderson94@gmail.com>
2021-07-26 13:06:23 +0100

    custom search filter scrapped. going for meets expectation

--- js/app.js
+++ js/app.js
@@ -1,14 +1,2 @@
 baguetteBox.run('.gallery');
-
-let input
-let filter 
-let pic
-let caption
-
-input = document.getElementById('myInput').addEventListener(InputEvent, console.log());
-
-
-//timer for help
-setTimeout(function() { alert("my message"); }, 2000);
-console.log(input)
-
+const search = new Filter('search', 'data-caption');
