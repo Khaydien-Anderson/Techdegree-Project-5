b5fb8993ea205627ddfb7d19ddd3a4af7c6016b0
K Andy <kdnanderson94@gmail.com>
2021-07-26 14:53:45 +0100

    Default script added back

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
 baguetteBox.run('.gallery');
-//const search = new Filter('search', 'data-caption');
-const search = document.querySelector( '#search' )
+const search = new Filter('search', 'data-caption'); 
 
-const captionss = document.querySelectorAll( '.gallery a' )
-
-const handleSearch = event => {
-    const searchTerm = event.target.value.toLowerCase();
-
-    captionss.forEach(caption => {
-const text = caption.textContent.toLowerCase();
-const pic = caption.parentElement;
-
-if (text.includes(searchTerm)) {
-    pic.style.display = "block";
-} else {
-    pic.style.display = "none"
-}
-    });
-}
-
-search.addEventListener('keyup', handleSearch);
