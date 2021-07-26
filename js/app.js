aca14fb2ed1e21f129c819eeda516a934b955017
K Andy <kdnanderson94@gmail.com>
2021-07-26 14:12:43 +0100

    RE-attempt of building search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+//const search = new Filter('search', 'data-caption');
+const search = document.querySelector( '#search' )
+
+const captionss = document.querySelectorAll( '.gallery a' )
+
+const handleSearch = event => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    captionss.forEach(caption => {
+const text = caption.textContent.toLowerCase();
+const pic = caption.parentElement;
+
+if (text.includes(searchTerm)) {
+    pic.style.display = "block";
+} else {
+    pic.style.display = "none"
+}
+    });
+}
+
+search.addEventListener('keyup', handleSearch);
